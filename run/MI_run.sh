30eded0c68c9d7032ef16b81a9e7b574adb9a749
Mathieu Rita <mrita@Mac-Rita.local>
2022-01-20 17:23:31 +0100

    init

--- run/MI_run.sh
+++
@@ -1,3 +0,0 @@
-python -m src.zoo.populations.train --population_json "/Users/mrita/Desktop/Work/PhD/Population/json/MI_expe/population.json" --print_info_population 1 --agents_json "/Users/mrita/Desktop/Work/PhD/Population/json/MI_expe/agents.json" --game_json "/Users/mrita/Desktop/Work/PhD/Population/json/onehotcompositionality_game.json" --training_json "/Users/mrita/Desktop/Work/PhD/Population/json/MI_expe/training.json" \
---log_dir "/Users/mrita/Desktop/Work/PhD/Population/logdir/MI_expe_2/1-2" \
---metrics_save_dir "/Users/mrita/Desktop/Work/PhD/Population/metrics/MI/1-2" \
