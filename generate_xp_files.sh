6812fa5ed47768f6802545627394e98aa7ca9d9e
Mathieu Rita <mrita@Mac-Rita.local>
2022-01-26 17:03:55 +0100

    rm bash files

--- generate_xp_files.sh
+++
@@ -1,3 +0,0 @@
-python -m src.zoo.populations.train --population_json "/Users/mrita/Desktop/Work/PhD/Population/json/MI_expe/population.json" --print_info_population 1 --agents_json "/Users/mrita/Desktop/Work/PhD/Population/json/MI_expe/agents.json" --game_json "/Users/mrita/Desktop/Work/PhD/Population/json/onehotcompositionality_game.json" --training_json "/Users/mrita/Desktop/Work/PhD/Population/json/MI_expe/training.json" \
---log_dir "/Users/mrita/Desktop/Work/PhD/Population/logdir/MI_expe_n/test_n" \
---metrics_save_dir "/Users/mrita/Desktop/Work/PhD/Population/metrics/MI/test_n" \
