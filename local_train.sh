d04b22e22340a1d25f865bec05e5bde24f24caf6
MathieuRita <uqm82td@jean-zay3.idris.fr>
2022-01-31 19:37:52 +0100

    rm

--- local_train.sh
+++
@@ -1,7 +0,0 @@
-python -m src.zoo.populations.train --population_json "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/json/population.json" \
-					--agents_json "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/json/agents.json" \
-					--game_json "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/json/game.json" \
-					--training_json "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/json/training.json" \
-					--log_dir "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/logs" \
-					--metrics_save_dir "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/metrics" \
-					--model_save_dir "/Users/mrita/Desktop/Work/PhD/Population/XP/5-5/models"
--- run/run.sh
+++
@@ -1,3 +0,0 @@
-python -m src.zoo.populations.train --population_json "/Users/mrita/Desktop/Work/PhD/Population/XP/6-6-mi/json/population.json" --print_info_population 1 --agents_json "/Users/mrita/Desktop/Work/PhD/Population/XP/6-6-mi/json/agents.json" --game_json "/Users/mrita/Desktop/Work/PhD/Population/XP/6-6-mi/json/game.json" --training_json "/Users/mrita/Desktop/Work/PhD/Population/XP/6-6-mi/json/training.json" \
---log_dir "/Users/mrita/Desktop/Work/PhD/Population/XP/6-6-mi/logs/expe_w_mi" \
---metrics_save_dir "/Users/mrita/Desktop/Work/PhD/Population/XP/6-6-mi/metrics/expe_w_mi" 
