f8d3b49c481cc925e3439a41d0d2af980ca63323
Mathieu Rita <mrita@Mac-Rita.local>
2022-02-18 00:07:03 +0100

    nn

--- src/core/games.py
+++ src/core/games.py
@@ -152,7 +152,7 @@ class ReconstructionGame(nn.Module):
 
         reward = th.log(p_x) + (log_prob_sender.detach() * mask_eos).sum(dim=1) - th.log(prob_lm).detach()
 
-        loss = - reward * log_prob_sender
+        loss = - reward * (log_prob_sender * mask_eos).sum(dim=1)
 
         #loss = agent_sender.tasks[task]["loss"].compute(reward=reward.detach(),
         #                                                sender_log_prob=log_prob_sender,
