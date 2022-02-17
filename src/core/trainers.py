d79adbf9ae411bfbfc6505570dcb3c3fcb98f14a
Mathieu Rita <mrita@mac-rita.home>
2022-02-17 12:42:00 +0100

    new language model

--- src/core/trainers.py
+++ src/core/trainers.py
@@ -281,7 +281,7 @@ class Trainer:
                 inputs = batch.data.to(self.device)
                 inputs_embedding = agent_sender.encode_object(inputs)
                 messages, _, _ = agent_sender.send(inputs_embedding)
-                messages_lm.append(messages)
+                messages_lm.append(messages.argmax(2)) #one-hot to index
             messages_lm = torch.stack(messages_lm).view(-1, messages_lm[0].size(1))
 
             agent_sender.train_language_model(messages_lm)
