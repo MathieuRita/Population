8ae5ade7f7c01a62f052cf286e219a17f216c1fd
Mathieu Rita <mrita@Mac-Rita.local>
2022-03-08 17:51:17 +0100

    opt list

--- src/core/trainers.py
+++ src/core/trainers.py
@@ -260,7 +260,7 @@ class TrainerBis:
 
         return mean_loss_senders, mean_loss_receivers, mean_metrics
 
-    def pretrain_optimal_listener(self, epoch: int, reset: bool = False, threshold=1e-3):
+    def pretrain_optimal_listener(self, epoch: int, reset: bool = True, threshold=1e-3):
 
         # Reset optimal listener
         if reset:
@@ -276,10 +276,11 @@ class TrainerBis:
             agent_sender = self.population.agents[sender_id]
             optimal_listener_id = agent_sender.optimal_listener
             optimal_listener = self.population.agents[optimal_listener_id]
-
-            model_parameters = list(optimal_listener.receiver.parameters()) + \
-                               list(optimal_listener.object_decoder.parameters())
-            optimal_listener.tasks["communication"]["optimizer"] = th.optim.Adam(model_parameters, lr=0.0005)
+            
+            if not reset:
+                model_parameters = list(optimal_listener.receiver.parameters()) + \
+                                   list(optimal_listener.object_decoder.parameters())
+                optimal_listener.tasks["communication"]["optimizer"] = th.optim.Adam(model_parameters, lr=0.0005)
 
         prev_loss_value = [0.]
         step=0
