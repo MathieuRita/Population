0e3eb9de6bde458f59efcf9ea1e5d0366b4ab0e3
Mathieu Rita <mrita@Mac-Rita.local>
2022-03-10 23:55:26 +0100

    opt list

--- src/core/games.py
+++ src/core/games.py
@@ -127,7 +127,7 @@ class ReconstructionGame(nn.Module):
         average_reward /= sum([v for _, v in weight_receivers.items()])
 
         if reward_noise:
-            average_reward+=th.normal(th.zeros(average_reward.size(0))).to(average_reward.device)
+            average_reward+=0.5*th.normal(th.zeros(average_reward.size(0))).to(average_reward.device)
 
         loss_sender = agent_sender.tasks[task]["loss"].compute(reward=average_reward,
                                                                sender_log_prob=log_prob_sender,
