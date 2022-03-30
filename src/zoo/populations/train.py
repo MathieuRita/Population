2608bc8d06223355b8a2e0ee5274e11cfd7f4d5b
Mathieu Rita <mrita@mac-rita.home>
2022-03-30 11:01:38 +0200

    corr

--- src/zoo/populations/train.py
+++ src/zoo/populations/train.py
@@ -171,7 +171,7 @@ def main(params):
                   train_communication_freq=training_params["train_communication_freq"],
                   train_broadcasting_freq=training_params["train_broadcasting_freq"],
                   train_imitation_freq=training_params["train_imitation_freq"],
-                  train_mi_freq=training_params["train_mi_freq"],
+                  train_custom_freq=training_params["train_custom_freq"],
                   train_kl_freq=training_params["train_kl_freq"],
                   validation_freq=training_params["validation_freq"],
                   evaluator_freq=training_params["evaluator_freq"],
