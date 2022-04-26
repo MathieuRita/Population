f0f308a5586b3f54d54debc85cd6202c539645fb
Mathieu Rita <mrita@Mac-Rita.local>
2022-04-26 17:54:05 +0200

    mi eval update

--- src/core/evaluators.py
+++ src/core/evaluators.py
@@ -185,9 +185,9 @@ class Evaluator:
                         mean_loss_value /= n_batch
                         step += 1
 
-                        #if (len(prev_loss_value) > 9 and abs(mean_loss_value - np.mean(prev_loss_value)) < 10e-4) or \
-                        #        step > 200:
-                        if step == 2500:
+                        if (len(prev_loss_value) > 9 and abs(mean_loss_value - np.mean(prev_loss_value)) < 10e-4) or \
+                                step > 200:
+                        #if step == 2500:
                             continue_optimal_listener_training = False
                             prev_loss_value.append(mean_loss_value)
                             prev_loss_value.pop(0)
