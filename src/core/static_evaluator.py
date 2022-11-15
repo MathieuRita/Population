ef2b422c8a8d0c7cc15e63d66f7be5a77c1ac87e
Mathieu Rita <mrita@Mac-Rita.local>
2022-11-15 17:39:02 +0100

    topsim general

--- src/core/static_evaluator.py
+++ src/core/static_evaluator.py
@@ -1073,7 +1073,7 @@ class StaticEvaluatorImage:
                     tot_distances_messages[f'{couple["sender"]}_{couple["receiver"]}'] = list()
                     tot_distances_projections[f'{couple["sender"]}_{couple["receiver"]}'] = list()
 
-                    test_set = [th.load(f"{self.dataset_dir}/{f}") for f in os.listdir(self.dataset_dir) if "test" in f]
+                    test_set = [th.load(f"{self.dataset_dir}/{f}") for f in os.listdir(self.dataset_dir) if "train" in f]
 
                     dataset = test_set
 
