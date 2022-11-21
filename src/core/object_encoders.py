0609dd560878fe521060d2dc31115c5bbb56fc04
Mathieu Rita <mrita@mac-rita.home>
2022-11-21 14:09:54 +0100

    broadcasting

--- src/core/object_encoders.py
+++ src/core/object_encoders.py
@@ -119,12 +119,12 @@ def build_object_projector(object_params: dict,
         if projection_size == -1:
             projector = nn.Identity()
         else:
-            #projector = nn.Linear(object_params["n_logits"], projection_size)
+            projector = nn.Linear(object_params["n_logits"], projection_size)
             W1 = nn.Linear(object_params["n_logits"], projection_size)
             A1 = nn.ReLU()
-            W2 = nn.Linear(projection_size, projection_size)
-            projector = nn.Sequential(W1, A1, W2)
-            
+            #W2 = nn.Linear(projection_size, projection_size)
+            projector = nn.Sequential(W1, A1)
+
             for param in projector.parameters():
                 param.requires_grad = False
     else:
