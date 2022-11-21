8a84bd3b0fed6331e0eeb5842aec066b7b97e3fb
Mathieu Rita <mrita@mac-rita.home>
2022-11-21 07:38:04 +0100

    broadcasting

--- src/core/agents.py
+++ src/core/agents.py
@@ -67,7 +67,8 @@ class Agent(object):
 
     def project_object(self,object):
         out = self.object_projector(object)
-        out = nn.ReLU(out)
+        m = nn.ReLU()
+        out = m(out)
         return out
 
     def compute_referential_scores(self,
